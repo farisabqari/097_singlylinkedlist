450828a45c823a9cab24b71c4a5154d29ab9c8f7
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-19 19:31:07 +0800

    memasukan case 4

--- lingkedListOOP/lingkedListOOP.cpp
+++ lingkedListOOP/lingkedListOOP.cpp
@@ -161,4 +161,26 @@ int main()
 		{
 			mhs.traverse();
 		}
+		break;
+
+		case '4':
+		{
+			if (mhs.listEmpty() == true)
+			{
+				cout << "\nList Kosong\n";
+				break;
+			}
+			Node* previous, * current;
+			cout << endl << "Masukan no mahasiswa yang dicari : ";
+			cin >> nim;
+			if (mhs.Search(nim, &previous, &current) == false)
+				cout << endl << "Data tidak ditemukan" << endl;
+			else
+			{
+				cout << endl << "Data ditemukan" << endl;
+				cout << "\nNo Mahasiswa: " << current->noMhs;
+				cout << "\nNama: " << current->nama;
+				cout << "\n";
+			}
+		}
 		
